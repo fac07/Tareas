954bfccf61317bc042d7a289f3110db840f3fb27
Cristian Castellanos <crcf85@gmail.com>
2020-02-04 10:30:33 -0600

    Carga de archivo tarea semana anterior

---
+++ 20002582Actividad2.js
@@ -0,0 +1,27 @@
+console.clear();
+
+var mes = 10;
+
+if(mes == 12 || mes == 1 || mes <= 2)
+{
+  console.log("Es invierno");
+}
+else if (mes >= 3 && mes <= 5)
+{
+  console.log("Es primavera");
+}
+else if (mes >= 6 && mes <= 8)
+{
+  console.log("Es verano");
+}
+else if (mes >= 9 && mes <= 11)
+{
+  console.log("Es otoño");
+}
+else
+{
+  console.log("El número de mes no es valido");
+}
+
+console.log("Nombre: Cristian René Castellanos Farfán");
+console.log("Carné: 20002582");
